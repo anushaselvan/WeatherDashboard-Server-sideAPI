01028eb4999cb9eabeb9c8eb9105d76482993982
Anusha <anushaselvan@gmail.com>
2021-09-29 00:08:10 +1000

    Fetching data from Open weather map API

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
  var formEl = $("#search-city");
+ var weatherEl = $("#weather");
  var cityNameEl = $("#city-name");
  var cityListEl = $("#city-list");
- var responseEl = $("p");
-
+ var responseEl = $("<p>");
 
  function printNames(name){
      //console.log("print names");
@@ -10,7 +10,30 @@
     cityListEl.append(listEl);
     formEl.append(cityListEl);
  }
- 
+ function currentWeather(name){
+   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=name&appid=8eeda8c33024abb85ba58c60e0b669ac";
+   fetch (requestUrl)
+       .then(function(response){
+           return response.json();
+   })
+   .then(function(data){
+       console.log(data);
+
+    weatherEl.addClass("brder");
+    var cityWeather = $("#weather").addClass("h2 font-weight-600").text(name);
+    var date = moment().format('l');
+    var currentDate = $("<p>").addClass("h3").text('('+ date +')');
+    var tempEl = $("<p>").addClass("h5").text('Temp:'+ " "+temp);
+   var windEl = $("<p>").addClass("h5").text("Wind:"+" "+wind);
+  //  var humidityEl = $("<p>").addClass("h5").text("Humidity:"+" "+ humid);
+    //var uvEl = $("<p>");
+    weatherEl.append( cityWeather, currentDate, tempEl, windEl);
+    //humidityEl);
+   });
+}
+
+
+
  
  
  function handleFormSubmit(event){
@@ -19,13 +42,15 @@
      var cityName = cityNameEl.val();
      if(!cityName){
          console.log("inside if loop");
-         responseEl.text("Plese enter a City name!");
+        responseEl.text("Please enter a City name!");
         formEl.append(responseEl);
          return;
      }
 
         printNames(cityName);
+        currentWeather(cityName);
         cityNameEl.val("");
+
     }
  
     formEl.on('submit', handleFormSubmit);
@@ -50,4 +75,4 @@
      $('#city-name').autocomplete({
          source:cityNames,
     });
-    });
+});
