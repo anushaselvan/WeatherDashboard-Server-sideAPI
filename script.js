9da88f0fdcfad298c6120151ec0b929c2b75da82
Anusha <anushaselvan@gmail.com>
2021-10-01 19:17:57 +1000

    Fetching data seperately for UV index

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
     formEl.append(cityListEl);
  }
  function currentWeather(name){
-   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=name&appid=8eeda8c33024abb85ba58c60e0b669ac";
+   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=8eeda8c33024abb85ba58c60e0b669ac&units=metric";
    fetch (requestUrl)
        .then(function(response){
            return response.json();
@@ -22,16 +22,36 @@
     weatherEl.addClass("brder");
     var cityWeather = $("#weather").addClass("h2 font-weight-600").text(name);
     var date = moment().format('l');
-    var currentDate = $("<p>").addClass("h3").text('('+ date +')');
-    var tempEl = $("<p>").addClass("h5").text('Temp:'+ " "+temp);
-   var windEl = $("<p>").addClass("h5").text("Wind:"+" "+wind);
-  //  var humidityEl = $("<p>").addClass("h5").text("Humidity:"+" "+ humid);
-    //var uvEl = $("<p>");
-    weatherEl.append( cityWeather, currentDate, tempEl, windEl);
-    //humidityEl);
-   });
-}
+    var currentDate = $("<p>").addClass("h4").text('('+ date +')');
+    var tempEl = $("<p>").addClass("h6").text('Temp:'+ " "+ data.main.temp+"°C");
+    var windEl = $("<p>").addClass("h6").text("Wind:"+" "+ data.wind.speed+"km/h");
+    var humidityEl = $("<p>").addClass("h6").text("Humidity:"+" "+ data.main.humidity+"%");
+   
+    
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid=8eeda8c33024abb85ba58c60e0b669ac&units=metric")
+   .then(function(response){
+       return response.json();
+})
+.then(function(data){
+   console.log(data);
+    
+    var uvEl= $("<p>").addClass("h6").text("UV Index:"+ " "+data.current.uvi);
+    if(data.current.uvi < 2){
+    uvEl.addClass("favourable"); 
+    }
+    if(data.current.uvi > 2 && data.current.uvi < 5)
+    {
+        uvEl.addClass("moderate"); 
+    }
+    if(data.current.uvi > 5)
+    {
+        uvEl.addClass("high"); 
+    }
+   weatherEl.append( cityWeather, currentDate, tempEl, windEl, humidityEl,uvEl);
 
+  });  
+});
+ }
 
 
  
@@ -41,7 +61,6 @@
      console.log("you are here");
      var cityName = cityNameEl.val();
      if(!cityName){
-         console.log("inside if loop");
         responseEl.text("Please enter a City name!");
         formEl.append(responseEl);
          return;
